37853cecfe04d1d7fa07fce5bac64ebbd1af0069
Steve Lin <sl.lin@advantech.com.tw>
2024-12-25 16:12:40 +0800

    add tvm files which was splited from the tvm-0.15.0-cp310-cp310-linux_aarch64.whl

--- pre_install/split-cat-tvm.sh
+++ pre_install/split-cat-tvm.sh
@@ -7,5 +7,5 @@
 cat tvm-0.15.0-cp310-cp310-linux_aarch64.whl.part* > tvm-0.15.0-cp310-cp310-linux_aarch64.whl
 
 #to be removed
-#rm -f tvm-0.15.0-cp310-cp310-linux_aarch64.whl.part*
+rm -f tvm-0.15.0-cp310-cp310-linux_aarch64.whl.part*
 
