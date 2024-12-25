cd8f356212e8d5bfc8238c65449a32a46dec6746
Steve Lin <sl.lin@advantech.com.tw>
2024-12-25 16:06:31 +0800

    faild to push 714MB tvm whl, workaround it by a script - split-cat-tvm.sh

---
+++ pre_install/split-cat-tvm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#split tvm
+#split -b 100M tvm-0.15.0-cp310-cp310-linux_aarch64.whl "tvm-0.15.0-cp310-cp310-linux_aarch64.whl.part"
+
+#combine all parts togeter
+cat tvm-0.15.0-cp310-cp310-linux_aarch64.whl.part* > tvm-0.15.0-cp310-cp310-linux_aarch64.whl
+
+#to be removed
+#rm -f tvm-0.15.0-cp310-cp310-linux_aarch64.whl.part*
+
